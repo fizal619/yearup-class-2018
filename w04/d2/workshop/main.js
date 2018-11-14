b21bb56e4518378ace4f03f08032e0b3434a6740
Kamran Uddin <kuddin@nyc.yearup.org>
2018-11-14 16:42:50 -0500

    tried workshop

--- w04/d2/workshop/main.js
+++ w04/d2/workshop/main.js
@@ -1,1 +1,26 @@
 console.log('🚀');
+
+axios.get
+  ("https://www.redbullshopus.com/products.json")
+    .then(function(response){
+    //  console.log(tData)
+
+      let productOne=response.data.products[0].images[0].src;
+      console.log(productOne);
+
+      let pic1 =document.getElementById('pic');
+      pic1.src=response.data.products[0].images[0].src;
+
+      let pic2 =document.getElementById('pic2');
+      pic2.src=response.data.products[0].images[1].src;
+
+      let pic3 =document.getElementById('pic3');
+      pic3.src=response.data.products[0].images[2].src;
+
+      
+
+    //  document.getElementsByTagName("img").src = response.data.products[0].images[0].src;
+    //  pic1.src=productOne;
+
+
+    });
