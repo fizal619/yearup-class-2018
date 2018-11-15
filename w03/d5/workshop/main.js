6e2a3efc94b9448df3747003c82e88b86e383e12
Andre Alcantara-Williams <aalcantarawillia@nyc.yearup.org>
2018-11-15 08:42:03 -0500

    day 5 workshop

--- w03/d5/workshop/main.js
+++ w03/d5/workshop/main.js
@@ -1,12 +1,25 @@
 class Art {
-  constructor(){
+  constructor(frame, url){
+    this.frame: "black-border",
+    this.url:'https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
 
-  }
+}
+
 
   render(){
     //do things to the DOM here
+let brandNew = document.createElement('img')
+
+function imageLoad(){
+    let image =  brandNew.src = this.url;
   }
+  brandNew.addEventListener('load', imageLoad);
+
+  }
+
 }
+
+let first = new Art()
 
 class Classical extends Art {
   constructor(){
